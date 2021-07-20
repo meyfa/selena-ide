24888d0995dadd9c9f0417896078d22bd2d7d4d4
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2021-07-20 14:55:25 +0200

    chore: Setup project
    This performs basic project setup.
    Webpack with TypeScript compilation is added, as well as
    linting with ESLint, and a GitHub Actions workflow.

---
+++ src/main.ts
@@ -0,0 +1,1 @@
+console.log('IDE started')
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.ts',
+  module: {
+    rules: [
+      { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.ts']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html')
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        path.resolve(__dirname, 'public', 'style.css')
+      ]
+    })
+  ]
+}
