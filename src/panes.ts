3f036e7c3ca43c50b451398024d3266bbfe41263
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2021-07-22 23:24:54 +0200

    feat: Implement resizing of input/output panes
    Adds a resizer element that can be used with any pointing device.
    When the resizer is dragged, the panes change size accordingly.

---
+++ src/panes.ts
@@ -0,0 +1,83 @@
+/**
+ * Minimum width of any pane as a fraction of the viewport width.
+ */
+const MINIMUM_PANE_WIDTH = 0.15
+
+/**
+ * Default resizer position, and where it snaps to, as a fraction of the viewport width.
+ */
+const PREFERRED_PANE_POSITION = 0.42
+
+/**
+ * Radius around the preferred position for which the pane size will snap to it.
+ */
+const SNAP_RADIUS = 0.01
+
+let inputPane: HTMLElement | undefined
+let outputPane: HTMLElement | undefined
+let resizerElement: HTMLElement | undefined
+
+let resizing = false
+
+function snapPosition (pos: number): number {
+  if (pos < MINIMUM_PANE_WIDTH) {
+    return MINIMUM_PANE_WIDTH
+  }
+  if (pos > (1 - MINIMUM_PANE_WIDTH)) {
+    return 1 - MINIMUM_PANE_WIDTH
+  }
+  // snap to preferred position
+  if (Math.abs(pos - PREFERRED_PANE_POSITION) < SNAP_RADIUS) {
+    return PREFERRED_PANE_POSITION
+  }
+  return pos
+}
+
+function startResize (): void {
+  resizing = true
+}
+
+function stopResize (): void {
+  resizing = false
+}
+
+function setPosition (position: number): void {
+  if (inputPane != null && outputPane != null) {
+    inputPane.style.width = `${position * 100}%`
+    outputPane.style.width = `${(1 - position) * 100}%`
+  }
+}
+
+function resizeToPosition (position: number): void {
+  if (resizing) {
+    const pos = snapPosition(position)
+    setPosition(pos)
+  }
+}
+
+export function setupPanes (input: HTMLElement, output: HTMLElement, resizer: HTMLElement): void {
+  inputPane = input
+  outputPane = output
+  resizerElement = resizer
+
+  setPosition(PREFERRED_PANE_POSITION)
+
+  resizerElement?.addEventListener('pointerdown', event => {
+    event.preventDefault()
+    startResize()
+  })
+
+  window.addEventListener('pointerup', event => {
+    if (resizing) {
+      event.preventDefault()
+      stopResize()
+    }
+  })
+
+  window.addEventListener('pointermove', (event) => {
+    if (resizing) {
+      event.preventDefault()
+      resizeToPosition(event.clientX / document.body.clientWidth)
+    }
+  })
+}
