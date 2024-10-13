5198afe74e3ee1941d7c9db4f4cccc20fd5c70e2
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2024-10-13 20:46:58 +0200

    chore: Update to ESLint v9 and new config (#367)
    * chore: Update to ESLint v9 and new config
    
    * chore: Use Node.js 20 for linting in CI
    
    * chore: Update dev dependencies

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+import eslintConfig from '@meyfa/eslint-config'
+
+export default [
+  ...eslintConfig,
+  {
+    ignores: [
+      'dist',
+      '.idea',
+      '.vscode',
+      'coverage',
+      '*.js'
+    ]
+  }
+]
